049b6056e8e28d8b39798cb4f511d875923ef7a7
ZHX <zhonghx@hydefi.io>
2022-01-20 14:01:16 +0800

    reinit

--- src/main/java/io/datajek/springbootdemo/recommenderapi/RecommenderApiApplication.java
+++ src/main/java/io/datajek/springbootdemo/recommenderapi/RecommenderApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecommenderApiApplication {
+public class RecommenderapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecommenderApiApplication.class, args);
+		SpringApplication.run(RecommenderapiApplication.class, args);
 	}
 
 }
